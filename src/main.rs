b5ae0b6416d67ca32d61c7b8c5bbf84ce29af7af
Robin Mueller <robin.mueller.m@gmail.com>
2024-10-12 12:53:52 +0200

    separate smart LED module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![no_std]
 #![no_main]
-
-mod async_test;
+pub mod smart_led_async;
 
 use core::cell::Cell;
 use core::cell::RefCell;
-use core::slice::IterMut;
 
 use critical_section::Mutex;
 use defmt::println;
@@ -16,15 +14,8 @@ use embassy_sync::channel::Channel;
 use embassy_time::{Duration, Ticker};
 use embedded_hal::digital::InputPin;
 use esp_backtrace as _;
-use esp_hal::clock::Clocks;
 use esp_hal::gpio;
-use esp_hal::gpio::PeripheralOutput;
-use esp_hal::peripheral::Peripheral;
-use esp_hal::rmt::asynch::TxChannelAsync;
-use esp_hal::rmt::PulseCode;
 use esp_hal::rmt::Rmt;
-use esp_hal::rmt::TxChannelConfig;
-use esp_hal::rmt::TxChannelCreatorAsync;
 use esp_hal::time;
 use esp_hal::time::Instant;
 use esp_hal::Async;
@@ -33,7 +24,6 @@ use esp_hal::{
     prelude::*,
     timer::timg,
 };
-use esp_hal_smartled::LedAdapterError;
 use esp_println as _;
 use infrared::protocol::nec::NecCommand;
 use infrared::receiver::time::InfraMonotonic;
@@ -171,18 +161,18 @@ async fn main(spawner: Spawner) {
                         }
                         Action::Zero => LED_CHANNEL
                             .sender()
-                            .try_send(LedCmd::SelectMode(LightModeCommand::Rainbow))
+                            .try_send(LedCmd::SelectMode(LightModeCommand::OneColor))
                             .unwrap(),
                         Action::One => {
                             LED_CHANNEL
                                 .sender()
-                                .try_send(LedCmd::SelectMode(LightModeCommand::OneColor))
+                                .try_send(LedCmd::SelectMode(LightModeCommand::Pulsing))
                                 .unwrap();
                         }
                         Action::Two => {
                             LED_CHANNEL
                                 .sender()
-                                .try_send(LedCmd::SelectMode(LightModeCommand::Pulsing))
+                                .try_send(LedCmd::SelectMode(LightModeCommand::Rainbow))
                                 .unwrap();
                         }
                         Action::Three => {
@@ -323,7 +313,8 @@ async fn led_task(
     let receiver = LED_CHANNEL.receiver();
     // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
     // be used directly with all `smart_led` implementations
-    let mut led_helper = SmartLedAdapterAsync::new(rmt.channel0, led_pin, rmt_buffer);
+    let mut led_helper =
+        smart_led_async::SmartLedAdapterAsync::new(rmt.channel0, led_pin, rmt_buffer);
     let mut ticker = Ticker::every(embassy_time::Duration::from_millis(LED_CMD_CHECK_FREQ_MS));
     let mut fine_ticker = Ticker::every(embassy_time::Duration::from_millis(LED_FINE_TICKER_FREQ));
     let divisor = LED_CMD_CHECK_FREQ_MS / LED_FINE_TICKER_FREQ;
@@ -572,125 +563,5 @@ impl RainbowParameters {
 
     pub fn increment(&mut self) {
         self.scaling.increment();
-    }
-}
-
-const SK68XX_CODE_PERIOD: u32 = 1200;
-const SK68XX_T0H_NS: u32 = 320;
-const SK68XX_T0L_NS: u32 = SK68XX_CODE_PERIOD - SK68XX_T0H_NS;
-const SK68XX_T1H_NS: u32 = 640;
-const SK68XX_T1L_NS: u32 = SK68XX_CODE_PERIOD - SK68XX_T1H_NS;
-
-/// Adapter taking an RMT channel and a specific pin and providing RGB LED
-/// interaction functionality using the `smart-leds` crate
-pub struct SmartLedAdapterAsync<Tx, const BUFFER_SIZE: usize> {
-    channel: Tx,
-    rmt_buffer: [u32; BUFFER_SIZE],
-    pulses: (u32, u32),
-}
-
-impl<'d, Tx: TxChannelAsync, const BUFFER_SIZE: usize> SmartLedAdapterAsync<Tx, BUFFER_SIZE> {
-    /// Create a new adapter object that drives the pin using the RMT channel.
-    pub fn new<C, O>(
-        channel: C,
-        pin: impl Peripheral<P = O> + 'd,
-        rmt_buffer: [u32; BUFFER_SIZE],
-    ) -> SmartLedAdapterAsync<Tx, BUFFER_SIZE>
-    where
-        O: PeripheralOutput + 'd,
-        C: TxChannelCreatorAsync<'d, Tx, O>,
-    {
-        let config = TxChannelConfig {
-            clk_divider: 1,
-            idle_output_level: false,
-            carrier_modulation: false,
-            idle_output: true,
-
-            ..TxChannelConfig::default()
-        };
-
-        let channel = channel.configure(pin, config).unwrap();
-        // Assume the RMT peripheral is set up to use the APB clock
-        let clocks = Clocks::get();
-        let src_clock = clocks.apb_clock.to_MHz();
-
-        Self {
-            channel,
-            rmt_buffer,
-            pulses: (
-                u32::from(PulseCode {
-                    level1: true,
-                    length1: ((SK68XX_T0H_NS * src_clock) / 1000) as u16,
-                    level2: false,
-                    length2: ((SK68XX_T0L_NS * src_clock) / 1000) as u16,
-                }),
-                u32::from(PulseCode {
-                    level1: true,
-                    length1: ((SK68XX_T1H_NS * src_clock) / 1000) as u16,
-                    level2: false,
-                    length2: ((SK68XX_T1L_NS * src_clock) / 1000) as u16,
-                }),
-            ),
-        }
-    }
-
-    pub async fn write(
-        &mut self,
-        led: impl IntoIterator<Item = RGB8>,
-    ) -> Result<(), LedAdapterError> {
-        self.prepare_rmt_buffer(led)?;
-        for chunk in self.rmt_buffer.chunks(25) {
-            self.channel
-                .transmit(chunk)
-                .await
-                .map_err(LedAdapterError::TransmissionError)?;
-        }
-        Ok(())
-    }
-
-    pub fn prepare_rmt_buffer(
-        &mut self,
-        iterator: impl IntoIterator<Item = RGB8>,
-    ) -> Result<(), LedAdapterError> {
-        // We always start from the beginning of the buffer
-        let mut seq_iter = self.rmt_buffer.iter_mut();
-
-        // Add all converted iterator items to the buffer.
-        // This will result in an `BufferSizeExceeded` error in case
-        // the iterator provides more elements than the buffer can take.
-        for item in iterator {
-            Self::convert_rgb_to_pulse(item, &mut seq_iter, self.pulses)?;
-        }
-        Ok(())
-    }
-
-    /// Converts a RGB value to the correspodnign pulse value.
-    pub fn convert_rgb_to_pulse(
-        value: RGB8,
-        mut_iter: &mut IterMut<u32>,
-        pulses: (u32, u32),
-    ) -> Result<(), LedAdapterError> {
-        Self::convert_rgb_channel_to_pulses(value.g, mut_iter, pulses)?;
-        Self::convert_rgb_channel_to_pulses(value.r, mut_iter, pulses)?;
-        Self::convert_rgb_channel_to_pulses(value.b, mut_iter, pulses)?;
-        *mut_iter.next().ok_or(LedAdapterError::BufferSizeExceeded)? = 0;
-
-        Ok(())
-    }
-
-    pub fn convert_rgb_channel_to_pulses(
-        channel_value: u8,
-        mut_iter: &mut IterMut<u32>,
-        pulses: (u32, u32),
-    ) -> Result<(), LedAdapterError> {
-        for position in [128, 64, 32, 16, 8, 4, 2, 1] {
-            *mut_iter.next().ok_or(LedAdapterError::BufferSizeExceeded)? =
-                match channel_value & position {
-                    0 => pulses.0,
-                    _ => pulses.1,
-                }
-        }
-
-        Ok(())
     }
 }
